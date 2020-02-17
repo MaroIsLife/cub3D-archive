b7a7a267e1297ad4f6b771de9eb6d9e4becbe46f
Marouane Ougnou <mougnou@e1r9p5.1337.ma>
2020-02-18 00:27:48 +0100

    Saving files

--- cub3d.h
+++ cub3d.h
@@ -8,10 +8,21 @@
 # include "libft/libft.h"
 # include "mlx.h"
 
+#define map_rows 23
+#define map_cols 27
+#define tile_size 32
+#define RED 0xff0000
+#define WHITE 0xffffff
+#define GOLD 0xffd700
+#define BLUE 0x0000ff
+#define AQUA 0x00ffff
+
+
 typedef struct s_mlx {
 		void *mlx_ptr;
 		void *win_ptr;
 }	t_mlx;
+
 t_mlx g_mlx;
 
 typedef struct s_data {
